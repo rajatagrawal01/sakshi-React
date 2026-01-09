5a47ff0597f9fef72752c395e9bcf6af6b25ef40
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-01-09 22:30:04 +0530

    js initial

---
+++ js/dataTypes.js
@@ -0,0 +1,18 @@
+// Number,string,boolean,null,undefined
+
+var a=25;
+var b=4.4;
+var c="Hello";
+var d=true;
+var e=null;
+var f;
+
+
+console.log("type of : a :",typeof(a));
+console.log("type of : b :",typeof(b));
+console.log("type of : c :",typeof(c));
+console.log("type of : d :",typeof(d));
+console.log("type of : e :",typeof(e));
+console.log("type of : f :",typeof(f));
+
+
---
+++ js/varTypes.js
@@ -0,0 +1,15 @@
+// const a =25;
+
+// console.log(a)
+
+
+let b=25;
+
+console.log("Value of b before scope : ",b);
+
+{
+    let b=30;
+    console.log("Value of b inside scope : ",b);
+}
+
+console.log("Value of b after scope : ",b);
