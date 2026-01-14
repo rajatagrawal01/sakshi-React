1bd2a54bbe1becf83dac2d377d953af37e3f6eb0
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-01-14 10:50:58 +0530

    functions and arrays

---
+++ js/arrays.js
@@ -0,0 +1,27 @@
+var arr=[1,2,3,4,5,6,7,8,9,10]
+
+// console.log(a[0]);
+
+
+// for(var i=0; i<a.length;i++){
+//     console.log(a[i]);
+// }
+
+
+// var a=arr.pop()
+// console.log(arr);
+
+// console.log(a);
+
+
+// arr.push(11)
+// console.log(arr);
+
+// arr.shift()
+
+// console.log(arr);
+
+// arr.unshift(0);
+// console.log(arr);
+
+
---
+++ js/functions.js
@@ -0,0 +1,45 @@
+// 1- Declaration- Definition
+
+// function Function_name(param if any){
+//     code
+// }
+
+// 2- Call - Use
+// Function_name();
+
+// No parameter
+// function welcome(){
+//     console.log("Welcome to JS Class");
+// }
+
+// function bye(){
+//     console.log("Bye from JS Class");
+// }
+// welcome();
+
+// var a=23;
+
+// if(a%2==0){
+//     welcome();
+// }
+// else{
+//     bye()
+// }
+
+
+// Parameter
+// function add(a,b){
+//     // console.log(a+b); --> non-returning
+//     return a+b --> returning
+// }
+
+// var a=add(23,45) * 2 //68
+// console.log(a);
+
+// Default Parameter
+
+// function welcome(a="Guest"){
+//     console.log("Welcome ",a," to JS Class");
+// }
+
+// welcome("Sakshi");
