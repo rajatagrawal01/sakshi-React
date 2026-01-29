2ff8af86873fb737f266d7feec5999cd17596fe3
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-01-29 19:53:04 +0530

    arrmethods

--- jsAdvance.js
+++ jsAdvance.js
@@ -57,4 +57,54 @@ var arr=[1,2,3]
 //     console.log(arr[i]*2);
 // }
 
-arr.forEach(i=>console.log(i*2))
+// ======================For Each=============================
+
+// arr.forEach(i=>console.log("ForEach: ",i*2))
+
+
+// ======================Map=============================
+
+// let doubled = arr.map((value)=>{
+//     return value*2;
+// }) 
+// doubled=[2,4,6]
+// console.log("Map",doubled);
+
+
+// ======================Filter=============================
+
+var arr=[1,2,3,5,4,8,7,234,123,34534,121]
+
+// let filteredArray = arr.filter((value)=>value>20);
+// console.log(filteredArray);
+
+// ======================Find=============================
+// let result = arr.find((value)=>value>20);
+
+// console.log(result);
+
+// ======================Some=============================
+
+// let example=arr.some((value)=>value>30000);
+// console.log(example);
+
+// ======================Every=============================
+
+// let allEven=arr.every((value)=>value%2==0)
+// let allEven=arr.every((value)=>value<50000)
+
+// console.log(allEven);
+
+
+// ======================Including=============================
+// let fruits=["apple","banana","watermelon"];
+
+// console.log(fruits.includes("banana"));
+
+// ======================sort()=============================
+arr.sort((a,b)=>a-b) // Ascending
+arr.sort((a,b)=>b-a) // Descending
+
+console.log(arr);
+
+
