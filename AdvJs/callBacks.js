6d5305b7a22b7ebf445bbd43a6333fc747352bb8
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-01-31 11:45:11 +0530

    callback and fetch

---
+++ AdvJs/callBacks.js
@@ -0,0 +1,34 @@
+// function welcome(StuName,callback){
+//     console.log(`Hello ${StuName}`);
+//     callback();
+// }
+
+// function bye(){
+//     console.log("Goodbye");
+// }
+
+// function greet(){
+//     console.log("How are you?");
+// }
+
+// welcome("Sakshi",bye)
+// welcome("Sakshi",greet)
+
+console.log("Start");
+
+// setTimeout(()=>{
+//     console.log("Timeout Function");
+// },5000)
+
+// console.log("End");
+
+// setInterval(()=>{
+//     console.log("Hello");
+// },1000)
+
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>response.json())
+.then((data)=>console.log(data))
+.catch((err)=>console.log("Error is :",err))
