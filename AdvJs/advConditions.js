872ac6a0b886ee515bbe37ef834df23913f03b90
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-02-17 11:03:47 +0530

    adv conditions and useState

---
+++ AdvJs/advConditions.js
@@ -0,0 +1,48 @@
+// if(condition){
+//     //operation
+// }
+// var result;
+// if(age>=18){
+//     result="Adult"
+// }
+// else{
+//     result = "Minor"
+// }
+
+const { useState } = require("react");
+
+
+// let result = age>=18 ? "Adult": "Minor"
+
+
+// // Logical operators as condition
+// let isLogged=true
+// if(isLogged){
+//     console.log("Welcome");
+// }
+
+// isLogged && console.log("Welcome");
+
+// // OR
+// let name = inputName || "Guest"
+
+// console.log();
+
+
+// function output(user){
+//     if(user.usrstatus!=="Active") return;
+//     console.log("Welcome");
+//     console.log("Your id is ....");
+// }
+// let userName={
+//     userNm:"Rajat",
+//     usrstatus:"Active"
+// }
+
+// output(userName)
+
+
+
+if(await checkUser()){
+    console.log("Enter");
+}
--- myapp/src/Hooks/StateHook.js
+++ myapp/src/Hooks/StateHook.js
@@ -2,27 +2,50 @@ import React, { useState } from "react";
 
 export default function StateHook() {
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
   const [visibility, setVisibility] = useState(true);
+  
+  const [name, setName] = useState("");
+  const [heading,setHeading] = useState("");
+  const [users,setUsers] = useState([])
+
+
 
   return (
     <div>
-      <h2>Count: {count}</h2>
+      {/* Number useState */}
+      {/* <h2>Count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(0)}>Reset</button> */}
 
-      <h3>Name : {name}</h3>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      {/* <button>Submit</button> */}
-
-    <br/>
-      {visibility && <p>Now you see</p>}
-      <button onClick={()=>setVisibility(!visibility)}>Click Here</button>
+      {/* String useState */}
+      <h3>Name : {heading}</h3>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+      <button onClick={()=>setHeading(name)}>Submit</button>
+      <br />
+      {/* Boolean useState */}
+      {/* {visibility && <p>Now you see</p>}
+      <button onClick={()=>setVisibility(!visibility)}>Click Here</button> */}
     </div>
   );
 }
+
+
+//[
+// {
+//   name:"",
+//   age:,
+//   city:,
+//   mob:
+// },
+// {
+//   name:"",
+//   age:,
+//   city:,
+//   mob:
+// },// {
+//   name:"",
+//   age:,
+//   city:,
+//   mob:
+// }]
