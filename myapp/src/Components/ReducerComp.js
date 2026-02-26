036be524a2e18dac4ed3699cd729c803e0e1934e
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-02-26 11:06:27 +0530

    fdf

--- myapp/src/Components/ReducerComp.js
+++ myapp/src/Components/ReducerComp.js
@@ -3,29 +3,27 @@ import React, { useReducer } from "react";
 export default function ReducerComp() {
   // const [count, setCount] = useState(0);
 
-const reducer =(state,action)=>{
-    // console.log("State:",state,"Action",action);
-    if(action.type=="Increase"){
-        console.log(action.type);
-        return state+1
-    }
-    if(action.type=="Decrease"){
-        // console.log(action.type);
-        return state-1
-    }
-}
 
+  const reducer = (state, action) => {
+    // console.log("State:",state,"Action",action);
+    if (action.type == "Increase") {
+      console.log(action.type);
+      return state + 1;
+    }
+    if (action.type == "Decrease") {
+      // console.log(action.type);
+      return state - 1;
+    }
+  };
 
   const [count, dispatch] = useReducer(reducer, 0);
   // const [count, dispatch] = useReducer(reducerFunction, initialState);
 
-
-  
   return (
     <div>
       <h2>Count: {count}</h2>
-      <button onClick={() => dispatch({type:"Increase"})}>Increment</button>
-      <button onClick={() => dispatch({type:"Decrease"})}>Decrement</button>
+      <button onClick={() => dispatch({ type: "Increase" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "Decrease" })}>Decrement</button>
       {/* <button onClick={() => setCount(0)}>Reset</button> */}
     </div>
   );
